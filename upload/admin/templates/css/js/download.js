c841cd01d3e62677eadaefc3612c6cd70d698576
f0restw0w <f0restjsf@gmail.com>
2013-12-28 03:14:06 -0800

    删除所有文件 ，重新构建版本库

--- upload/admin/templates/css/js/download.js
+++
@@ -1,6 +0,0 @@
-function set_src(){
-    var start=$("#start").val();
-    var end=$("#end").val();
-    url='download_frame.php?m=list&s='+start+'&end='+end+'&now='+start;
-    $("#downiframe").attr({"src":url});
-}
--- upload/upload/callback.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $params=$_GET['params'];
-    $params=preg_replace('~(\\\")~','"',$params);
-    echo('<script>window.parent.callback_upload(\''.$params.'\');</script>');
-?>
