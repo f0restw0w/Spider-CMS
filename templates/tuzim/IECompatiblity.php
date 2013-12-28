caa9115401e9081dc3e9be61c10340c81e8e8842
f0restw0w <f0restjsf@gmail.com>
2013-12-28 06:01:18 -0800

    增加一个模板，主页内容宽度增加到了1100px

---
+++ templates/tuzim/IECompatiblity.php
@@ -0,0 +1,6 @@
+<!--Css Compatiblily start-->
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<!--[if lte IE 8]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!--Css Compatiblily end-->
